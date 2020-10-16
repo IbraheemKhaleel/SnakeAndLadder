05b2f2039b3404b5775a71ccf792bcfd87e38932
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-10-16 10:48:24 +0530

    added code for winning positions . Here it is reaching 100

---
+++ WinPositionUC4.java
@@ -0,0 +1,41 @@
+package snakeAndLadder;
+
+import java.util.Random;
+
+public class WinPositionUC4 
+{
+	public static final int NOPLAY = 0 ;
+	public static final int LADDER = 1 ;
+	public static final int SNAKE = 2 ;
+	public static void main(String[] args) 
+	{
+		Random rand = new Random();
+		int position = 0 ;
+		int[] board = new int[101];
+		for( int index = 0 ; index<board.length ; index++ )
+		{
+			board[index] = index ;
+		}
+		while ( position <= 100 && position >= 0 )
+		{
+			int diceRoll = rand.nextInt(6) + 1;
+			int options = rand.nextInt(3);
+			if ( options == LADDER )
+			{
+				position = position + diceRoll ;
+			}
+			else if ( options == SNAKE )
+			{
+				position = position - diceRoll ;
+				if ( position < 0 )
+				{
+					position = 0 ;
+				}
+			}
+			else
+			{
+				position += 0 ;
+			}
+		}
+	}
+}
